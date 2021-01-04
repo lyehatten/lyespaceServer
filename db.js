5d23070544c2abe3a7aaeb2c41588c4a8f9bb7e7
lynqux <57960864+lynqux@users.noreply.github.com>
2021-01-04 10:50:50 -0500

    deploying!

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('lyespace', 'postgres', "WhatisUp!400", {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres'
 });
 
